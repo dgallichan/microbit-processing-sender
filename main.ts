c01d59d9c2a2e99875b32202a78b44f4a164eebb
Daniel Gallichan <dgallichan@yahoo.com>
2022-08-11 17:04:58 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ basic.showLeds(`
     # # # # #
     `)
 basic.forever(function () {
-    serial.writeLine("" + control.deviceName() + "," + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z)))
+    serial.writeString("" + control.deviceName() + "," + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z)) + "\n")
     led.toggle(2, 2)
-    basic.pause(25)
+    basic.pause(40)
 })
