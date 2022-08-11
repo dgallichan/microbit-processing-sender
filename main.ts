4408bc696488d466ff89cfe3202d3c26c92109d0
Daniel Gallichan <dgallichan@yahoo.com>
2022-08-11 17:01:15 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,8 +5,8 @@ basic.showLeds(`
     # . . . #
     # # # # #
     `)
-control.inBackground(function () {
+basic.forever(function () {
     serial.writeLine("" + control.deviceName() + "," + convertToText(input.acceleration(Dimension.X)) + "," + convertToText(input.acceleration(Dimension.Y)) + "," + convertToText(input.acceleration(Dimension.Z)))
-    basic.pause(40)
     led.toggle(2, 2)
+    basic.pause(25)
 })
